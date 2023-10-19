d622ea983d869268687ebc07ba5dfd1f469389b6
bank <ananda2546@gmail.com>
2023-10-20 02:01:43 +0700

    modify: book.route

--- app/routes/book.routes.js
+++ app/routes/book.routes.js
@@ -2,10 +2,9 @@ const authJwt = require("../middleware/auth.jwt");
 module.exports = (app)=>{
     const book_controller = require("../controller/book.controller");
     var router = require("express").Router();
-    router.get('/books', book_controller.getAllBook);
-    router.post('/addbook',book_controller.createBook);
+    router.get('/', book_controller.getAllBook);
+    router.post('/addbooks',book_controller.createBook);
     router.get('/:id',book_controller.getBookId);
     router.delete('/:id',book_controller.deleteBook)
-    app.use("/api", router);
-    app.use("/api/auth", router);
+    app.use("/api/books", router);
 }; 
